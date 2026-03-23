09b7ff3de975806c1d7784e3da6f0b7fe1beefc6
Toluwalase Bewaji <toluwalase.bewaji@yahoo.com>
2026-03-23 20:32:39 +0100

    feat: add App.vue with header, footer, and router view setup

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+import App_header from './components/app_header.vue'
+import App_footer from './components/app_footer.vue'
+// import App_container from './components/app_container.vue'
+</script>
+
+<template>
+  <!-- <App_container></App_container> -->
+  <App_header />
+  <RouterView />
+  <App_footer />
+</template>
